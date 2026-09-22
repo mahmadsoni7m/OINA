f8884b083cd33c96c65807191f8572181effa58a
mahmadsoni7m <mahmadsoni7@gmail.com>
2026-09-22 06:49:40 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,10 @@ android {
         }
     }
 }
+dependencies {
+    implementation("androidx.compose.foundation:foundation:1.6.0") // ё версияи ҷадидтар
+    implementation("androidx.compose.ui:ui:1.6.0")
+}
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
